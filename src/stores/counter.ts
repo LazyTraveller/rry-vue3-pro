e0c714e16f8dbacefe084336a4613b89280099e4
heweizhi <heweizhi@accompany.tech>
2024-04-08 14:16:42 +0800

    first commit

---
+++ src/stores/counter.ts
@@ -0,0 +1,13 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
+  const getCount = computed(() => count.value * 20)
+  function increment() {
+    count.value++
+  }
+
+  return { count, doubleCount, getCount, increment }
+})
